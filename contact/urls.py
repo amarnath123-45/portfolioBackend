16696a9c1b45196740e139a1aef2c9307f8a0ee4
Amarnath <amarnathtechfifo@gmail.com>
2026-06-08 11:07:02 +0530

    update

--- contact/urls.py
+++ contact/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import ContactListView, ContactView
+from .views import ContactListView, ContactView, test_smtp
 
 
 urlpatterns = [
 
     path('contact/', ContactView.as_view(), name='contact'),
     path("contact/list/", ContactListView.as_view(), name="contacts"),
+    path("smtp-test/", test_smtp),
 ]
